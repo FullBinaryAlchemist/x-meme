41fbfdb5c3984e79ccb049f0f885c3d9ccd5790b
divyanshu <dgdivyanshu@gmail.com>
2021-02-13 21:00:24 +0530

    test: working for /GET/:id and fixed /POST test

--- test/meme.js
+++ test/meme.js
@@ -4,6 +4,7 @@ let mongoose = require("mongoose");
 let Meme = require('../models/meme.model');
 
 let chai = require('chai');
+
 let chaiHttp = require('chai-http');
 let server = require('../server');
 let should = chai.should();
@@ -29,7 +30,7 @@ describe('Memes', () => {
             });
       });
   });
-  
+
   describe('/POST meme', () => {
       it('it should not POST a meme without url field', (done) => {
           let meme = {
@@ -38,7 +39,7 @@ describe('Memes', () => {
               //url is missing
           }
             chai.request(server)
-            .post('/meme')
+            .post('/memes')
             .send(meme)
             .end((err, res) => {
                   res.should.have.status(400);
@@ -46,70 +47,74 @@ describe('Memes', () => {
               done();
             });
       });
- //      it('it should POST a meme ', (done) => {
- //          let meme = {
- //              caption: "The Lord of the Rings",
- //              name: "J.R.R. Tolkien",
- //              url: "https://i.pinimg.com/originals/af/8d/63/af8d63a477078732b79ff9d9fc60873f.jpg"
- //          }
- //            chai.request(server)
- //            .post('/memes')
- //            .send(meme)
- //            .end((err, res) => {
- //                  res.should.have.status(200);
- //                  res.body.should.be.a('object');
- //                  //since we have only a single meme 
- //                  Meme.find(meme,(err,fetchedMemes) => res.body.should.have.property('id').eql(fetchedMemes[0].id))
+      it('it should POST a meme ', (done) => {
+          let meme = {
+              caption: "The Lord of the Rings",
+              name: "J.R.R. Tolkien",
+              url: "https://i.pinimg.com/originals/af/8d/63/af8d63a477078732b79ff9d9fc60873f.jpg"
+          }
+            chai.request(server)
+            .post('/memes')
+            .send(meme)
+            .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('object');
+                  //since we have only a single meme 
+                  Meme.find(meme,(err,fetchedMemes) => {res.body.should.have.property('id').eql(fetchedMemes[0].id);
+                    done();
 
-                  
-                  
- //              done();
- //            });
- //      });
- //  });
- //  describe('/GET/:id meme', () => {
- //      it('it should GET a meme by the given id', (done) => {
- //          let meme = new Meme({ caption: "The Lord of the Rings", name: "J.R.R. Tolkien", url: "https://i.pinimg.com/originals/af/8d/63/af8d63a477078732b79ff9d9fc60873f.jpg" });
- //          meme.save((err, meme) => {
- //              chai.request(server)
- //            .get('/memes/' + meme.id)
- //            .send(meme)
- //            .end((err, res) => {
- //                  res.should.have.status(200);
- //                  res.body.should.be.a('object');
- //                  res.body.should.have.property('caption');
- //                  res.body.should.have.property('name');
- //                  res.body.should.have.property('url');
- //                  res.body.should.have.property('id').eql(meme.id);
- //              done();
- //            });
- //          });
+                })
 
- //      });
- //  });
- //  describe('/PUT/:id meme', () => {
- //      it('it should UPDATE a meme given the id', (done) => {
- //          let meme = new Meme({title: "The Chronicles of Narnia", name: "C.S. Lewis", year: 1948, pages: 778})
- //          meme.save((err, meme) => {
- //                chai.request(server)
- //                .put('/meme/' + meme.id)
- //                .send({title: "The Chronicles of Narnia", name: "C.S. Lewis", year: 1950, pages: 778})
- //                .end((err, res) => {
- //                      res.should.have.status(200);
- //                      res.body.should.be.a('object');
- //                      res.body.should.have.property('message').eql('Meme updated!');
- //                      res.body.meme.should.have.property('year').eql(1950);
- //                  done();
- //                });
- //          });
- //      });
- //  });
+            });
+      });
+  });
+  describe('/GET/:id meme', () => {
+      it('it should GET a meme by the given id', (done) => {
+          let meme = new Meme({ caption: "The Lord of the Rings", name: "J.R.R. Tolkien", url: "https://i.pinimg.com/originals/af/8d/63/af8d63a477078732b79ff9d9fc60873f.jpg" });
+          meme.save((err, meme) => {
+              chai.request(server)
+            .get('/memes/' + meme.id)
+            .send(meme)
+            .end((err, res) => {
+                  res.should.have.status(200);
+                  res.body.should.be.a('object');
+                  res.body.should.have.property('caption');
+                  res.body.should.have.property('name');
+                  res.body.should.have.property('url');
+                  res.body.should.have.property('id').eql(meme.id);
+              done();
+            });
+          });
+
+      });
+  });
+  // describe('/PATCH/:id meme', () => {
+  //     it('it should UPDATE a meme given the id', (done) => {
+  //         let meme = new Meme({caption: "The Chronicles of Narnia", name: "C.S. Lewis", url:"https://i.pinimg.com/originals/af/8d/63/af8d63a477078732b79ff9d9fc60873f.jpg"})
+  //         meme.save((err, meme) => {
+  //               chai.request(server)
+  //               .patch('/memes/' + meme.id)
+  //               .send({caption: "The Lord of The Rings", name: "L.R. Tolkien", url:"https://www.google.com" })
+  //               .end((err, res) => {
+  //                     res.should.have.status(200);
+  //                     Meme.findById(meme.id,(err,fetchedMeme)=>{
+  //                       console.log(fetchedMeme);
+  //                       assert.equal(fetchedMeme.name,meme.name) //name  NOT changed 
+  //                       assert.equal(fetchedMeme.caption,"The Lord of  Rings") //caption changed
+  //                       assert.equal(fetchedMeme.url,"https://www.google.com") //url change
+
+  //                     })
+  //                 done();
+  //               });
+  //         });
+  //     });
+  // });
  // /*
  //  * Test the /DELETE/:id route
  //  */
  //  describe('/DELETE/:id meme', () => {
  //      it('it should DELETE a meme given the id', (done) => {
- //          let meme = new Meme({title: "The Chronicles of Narnia", name: "C.S. Lewis", year: 1948, pages: 778})
+ //          let meme = new Meme({caption: "The Chronicles of Narnia", name: "C.S. Lewis", year: 1948, pages: 778})
  //          meme.save((err, meme) => {
  //                chai.request(server)
  //                .delete('/meme/' + meme.id)
