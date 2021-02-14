19a82bc30e233da22cf289a831b112f5b382dc8c
divyanshu <dgdivyanshu@gmail.com>
2021-02-14 15:41:02 +0530

    add:Better error message handling

--- checkValidImageUrl.js
+++ checkValidImageUrl.js
@@ -1,15 +1,15 @@
 const fetch = require("node-fetch");
 function checkBlobtype(blob){
-	if (blob)
+	if (blob)//check if successfully converted into blob
 		return blob.type.includes("image/");
 	else
 		return false;
 }
 const getBlob=async (url) => {
 	try{
-	const response =await fetch(url);
-	const blob= await response.blob();
-	return blob;
+	const response =await fetch(url); //get the respone object
+	const blob= await response.blob(); //convert it to Blob
+	return blob; //return the promise
 	}catch(err){
 		console.log('getBlob Error:'+err);
 	}
