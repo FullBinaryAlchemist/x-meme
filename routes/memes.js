b690605fe5ebf4f04a4821788b8f8ebb773ca80e
divyanshu <dgdivyanshu@gmail.com>
2021-02-14 15:40:41 +0530

    feat: SWAGGER API added and fix:/PATCH fixed to work with just URL/CAPTION in reqBody

--- routes/memes.js
+++ routes/memes.js
@@ -1,7 +1,38 @@
 const router = require('express').Router();
 let Meme = require('../models/meme.model');
 const checkValidImgUrl = require("../checkValidImageUrl")
+/**
+ * @swagger
+ * definitions:
+ *  Meme:
+ *   type: object
+ *   properties:
+ *    name:
+ *     type: string
+ *     description: name of the author of the meme
+ *     example: 'Jayaramachandran'
+ *    caption:
+ *     type: string
+ *     description: caption of the meme
+ *     example: 'Look twice before you cross the road'
+ *    url:
+ *     type: string
+ *     description: An absolute url pointing to a "Content-type":"image/*"
+ *     example: 'https://static.toiimg.com/photo/72975551.cms'
+ */
 
+/**
+ * @swagger
+ * /memes:
+ *  get:
+ *   summary: get the latest 100 memes
+ *   description: get the latest 100 memes in reverse chronological order (that is latest meme will be at index 0 and oldest meme at last index)
+ *   responses:
+ *    200:
+ *     description: success. A JSON is returned containing the memes.An empty array is returned if no meme available
+ *    400:
+ *     description: error
+ */
 router.route('/').get((req, res) => {
   Meme.find()
     .then(memes => {
@@ -18,7 +49,24 @@ router.route('/').get((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
-router.route('/').post((req, res) => {
+/**
+  * @swagger
+  * /memes:
+  *  post:
+  *   summary: create a new meme
+  *   description: create a new meme and returns unique id of the newly created meme if meme is not duplicate . Otherwise returns error.
+  *   requestBody:
+  *    content:
+  *     application/json:
+  *      schema:
+  *       $ref: '#/definitions/Meme'
+  *   responses:
+  *    200:
+  *     description: meme created succesfully. Returns unique id of the newly created meme
+  *    409:
+  *     description: Duplicate meme(meme with same name , caption , and url) not allowed
+  */
+  router.route('/').post((req, res) => {
   const name = req.body.name;
   const caption = req.body.caption;
   const url = req.body.url
@@ -79,10 +127,51 @@ router.use('/:id', (req, res, next)=>{
 
 })
 
-
+/**
+ * @swagger
+ * /memes/{meme_id}:
+ *  get:
+ *   summary: get a MEME with given {meme_id}
+ *   description: get a MEME with given {meme_id}
+ *   parameters:
+ *    - in: path
+ *      name: meme_id
+ *      schema:
+ *       type: string
+ *      required: true
+ *      description: id of the meme
+ *      example: 6028ec4f8e42f82e88cc7245
+ *   responses:
+ *    200:
+ *     description: success
+ *    404:
+ *      description: meme does not exist
+ */
 router.route('/:id').get((req, res) => {
     res.json(req.meme.transform());
 });
+
+
+/**
+ * @swagger
+ * /memes/{meme_id}:
+ *  delete:
+ *   summary: delete a MEME with given {meme_id}
+ *   description: delete a MEME with given {meme_id}
+ *   parameters:
+ *    - in: path
+ *      name: meme_id
+ *      schema:
+ *       type: string
+ *      required: true
+ *      description: id of the meme
+ *      example: 6028ec4f8e42f82e88cc7245
+ *   responses:
+ *    209:
+ *     description: meme deleted successfully
+ *    404:
+ *      description: meme does not exist
+ */
 
 router.route('/:id').delete((req, res) => {
   Meme.findByIdAndDelete(req.meme.id)
@@ -90,10 +179,50 @@ router.route('/:id').delete((req, res) => {
     .catch(err => res.status(400).json('Error: ' + err));
 });
 
+/**
+ * @swagger
+ * /memes/{meme_id}:
+ *  patch:
+ *   summary: updates the URL or/and CAPTION of a meme with {meme_id}
+ *   description: update the URL and/or CAPTION of a MEME with given {meme_id}. NAME cannot be modified.Even if provided no change will be made
+ *   consumes:
+ *    - application/json
+ *   produces:
+ *    - application/json
+ *   parameters:
+ *    - in: path
+ *      name: meme_id
+ *      schema:
+ *       type: string
+ *      required: true
+ *      description: id of the meme
+ *      example: 6028ec4f8e42f82e88cc7245
+ *    - in: body
+ *      name: body
+ *      required: true
+ *      description: body object containg the name,caption, url
+ *      schema:
+ *       $ref: '#/definitions/Meme'
+ *   requestBody:
+ *    content:
+ *     application/json:
+ *      schema:
+ *       $ref: '#/definitions/Meme'  
+ *   responses:
+ *    200:
+ *     description: successly update meme
+ *    404:
+ *     description: meme does not exist
+ *    400:
+ *     description: error occurred when trying to update meme
+ */
+
 router.route('/:id').patch((req, res) => {
     
-      req.meme.caption = req.body.caption;
-      req.meme.url = req.body.url;
+      if(req.body.caption)
+        req.meme.caption=req.body.caption;
+      if(req.body.url)
+        req.meme.url= req.body.url;
       
       req.meme.save()
         .then(() => res.status(200).json('Meme updated!'))
--- server.js
+++ server.js
@@ -2,10 +2,38 @@ const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
 
+//adding SwaggerDocs
+const swaggerJsDoc = require("swagger-jsdoc");
+const swaggerUi = require("swagger-ui-express");
+
+
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT || 5000;
+
+const swaggerOptions = {
+  swaggerDefinition: {
+  	openapi:'3.0.0',
+    info: {
+      version: "1.0.0",
+      title: "Memes API",
+      description: "Meme API Information",
+      contact: {
+        name: "Divyanshu Gupta",
+        email: "dgdivyanshu@gmail.com"
+      },
+      servers: ["http://localhost:"+port]
+    }
+  },
+  
+  apis: ['./routes/*.js']
+};
+
+const swaggerDocs = swaggerJsDoc(swaggerOptions);
+app.use("/swagger-ui/", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
+
+
 
 app.use(cors());
 app.use(express.json());
