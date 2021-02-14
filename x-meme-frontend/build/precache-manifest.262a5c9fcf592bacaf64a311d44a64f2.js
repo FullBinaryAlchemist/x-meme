69bac446ed781df253c95e5e778b70170c05394a
divyanshu <dgdivyanshu@gmail.com>
2021-02-14 23:41:42 +0530

    fixed json added build

---
+++ x-meme-frontend/build/precache-manifest.262a5c9fcf592bacaf64a311d44a64f2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5b80941779cfcf9e9f6e37b9d9c2371a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fa57b7d12bb5eba8c773",
+    "url": "/static/css/2.ff645aaf.chunk.css"
+  },
+  {
+    "revision": "ba341cc9e1022834c129",
+    "url": "/static/css/main.cc6ecdc8.chunk.css"
+  },
+  {
+    "revision": "fa57b7d12bb5eba8c773",
+    "url": "/static/js/2.dce78761.chunk.js"
+  },
+  {
+    "revision": "ba341cc9e1022834c129",
+    "url": "/static/js/main.83bac452.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
